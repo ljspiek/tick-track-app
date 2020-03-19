5f28282a06fe28c586fbab3d8d890bfddb98c91e
Laura Spieker <ljspiek@gmail.com>
2020-03-18 20:37:19 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -18,12 +18,32 @@ class App extends Component {
           <h1>TickTrack</h1>
           <h2>Take control of your Lyme, one day at a time.</h2>
         </header>
-        <LandingPage/>
+        
       <main >
-        <SymptomLog/>
-        <SymptomSummary/>
-        <SignUp/>
-        <SignIn/>
+        <Route 
+          exact path='/'
+          component={LandingPage}
+        />
+        <Route
+          exact path='/log'
+          component={SymptomLog}
+        />
+        <Route
+          exact path='/summary'
+          component={SymptomSummary}
+        />
+        <Route
+          exact path='/log/:logId'
+          component={SymptomLog}
+        />
+        <Route
+          exact path='/signUp'
+          component={SignUp}
+        />
+        <Route
+          exact path='/signIn'
+          component={SignIn}
+        />
       </main>
       <footer>
         footer
