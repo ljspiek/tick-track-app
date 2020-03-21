c36d9e455ec97adee5b75a73488db114eb828469
Laura Spieker <ljspiek@gmail.com>
2020-03-21 10:17:12 -0500

    conditional rendering of new symptom indicators

--- src/SymptomLogDetail/SymptomLogDetail.js
+++ src/SymptomLogDetail/SymptomLogDetail.js
@@ -11,6 +11,9 @@ export default class SymptomLogDetail extends Component {
             log => { return log.id === this.props.match.params.logId }
             )
         
+        const newInfection = log.newinfectionindicators.length
+        
+
         
         return (
             <div>
@@ -19,7 +22,14 @@ export default class SymptomLogDetail extends Component {
                     <section>
                         <h3>{log.date}</h3>
                         <h4>Overall Health: {log.generalhealth.rating}</h4>
-                        <h4>New Infection Indicators: {log.newinfectionindicators.length}</h4>
+                        {newInfection > 0 && 
+                        <h4>New Infection Indicators:</h4>
+                        }
+                        {log.newinfectionindicators.map(indicators =>
+                            <ul key={indicators.id}>
+                                <li>{indicators.indicator}</li>
+                            </ul>
+                        )}
                         <h4>Symptoms Logged:</h4>
                         {log.symptoms.map(symptoms =>
                             <ul key={symptoms.id}>
@@ -27,6 +37,9 @@ export default class SymptomLogDetail extends Component {
                             </ul>
                             )}
                         <Link to='/summary'><button>Close</button></Link>
+                        {/* TO DO:Make Close into 'x' at top */}
+                        <Link to={`/log/${log.id}/edit`}><button>Edit</button></Link>
+                        {/* TO DO: Pencil icon - mobile only? */}
                         <button>Delete</button>
                     </section>
                     
